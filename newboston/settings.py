62b5302c541d9d5a03e7ac37020b13b9da4b1f53
supriadi-yusuf <supriadi96@gmail.com>
2018-07-17 19:58:51 +0700

    set connecting to postgres database

--- newboston/settings.py
+++ newboston/settings.py
@@ -72,14 +72,24 @@ WSGI_APPLICATION = 'newboston.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+'''
+DATABASES = { # modified by supri
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'newboston',
+        'USER': 'postgres',
+        'PASSWORD': 'pass123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
