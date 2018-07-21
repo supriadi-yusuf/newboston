c72b418ee5189c73aac8f01eb92983b53011d219
supriadi-yusuf <supriadi96@gmail.com>
2018-07-21 21:48:06 +0700

    add detail view for album in music app

--- music/views.py
+++ music/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 #from django.shortcuts import render
 
+# view is simple function/class that returns html
 def index(request):
     return HttpResponse("<h1>This is the music app homepage</h1>")
+
+def detail(request,id):
+    return HttpResponse("<h1>Detail for music with id=" + str(id) + "</h1>")
