be5a9d81cadf9b1c46b217b4dbcf560a80873446
supriadi-yusuf <supriadi96@gmail.com>
2018-07-21 21:58:59 +0700

    fix error in music.views by change parameter name from id to album_id refers to music.urls

--- music/views.py
+++ music/views.py
@@ -5,5 +5,5 @@ from django.http import HttpResponse
 def albumList(request):
     return HttpResponse("<h1>This is the music app homepage</h1>")
 
-def albumDetail(request,id):
-    return HttpResponse("<h1>Detail for music with id=" + str(id) + "</h1>")
+def albumDetail(request, album_id):
+    return HttpResponse("<h1>Detail for music with id=" + str(album_id) + "</h1>")
