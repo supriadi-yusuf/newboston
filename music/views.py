536f698622241fc6f81796a9a6619557750f31fb
supriadi-yusuf <supriadi96@gmail.com>
2018-07-21 21:49:49 +0700

    change function's name in music.views to make it more clear

--- music/views.py
+++ music/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 #from django.shortcuts import render
 
 # view is simple function/class that returns html
-def index(request):
+def albumList(request):
     return HttpResponse("<h1>This is the music app homepage</h1>")
 
-def detail(request,id):
+def albumDetail(request,id):
     return HttpResponse("<h1>Detail for music with id=" + str(id) + "</h1>")
