a96c2bfaaa771174c9e90f6ed3211315eaf1fb71
supriadi-yusuf <supriadi96@gmail.com>
2018-07-21 17:00:15 +0700

    register model Album and Song to admin. So they can be accessed through admin page

--- music/admin.py
+++ music/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Album, Song
+
+# Register your models here, so it can be accessed via admin page
+admin.site.register(Album) # register Album model
+admin.site.register(Song) # register Song model
