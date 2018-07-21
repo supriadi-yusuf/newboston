01ca102adfdaaf9e9a14d504200186e2163afc30
supriadi-yusuf <supriadi96@gmail.com>
2018-07-21 21:54:14 +0700

    add url to album detail in music.urls

--- music/urls.py
+++ music/urls.py
@@ -6,6 +6,7 @@ app_name = 'music_app' # add name space to url name for this app
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
     #url(r'^music/', include('music.urls')),
-    url(r'^$', views.index, name='album_list')
+    url(r'^$', views.albumList, name='album_list'),
+    url(r'^(?P<album_id>\d+)$', views.albumDetail, name='album_detail'),
 
 ]
