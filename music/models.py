360a32d8aa2eed7288774becc563f1f51465d6e4
supriadi-yusuf <supriadi96@gmail.com>
2018-07-21 12:18:22 +0700

    add Album, Song model to music app

--- music/models.py
+++ music/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+Class Album( models.Model):
+    artis = models.CharField(max-length=250)
+    title = models.CharField(max-length=500)
+    genre = models.CharField(max-length=100)
+    logo = models.CharField(max-length=1000)
+
+Class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    file_type = models.CharField(max-length=10)
+    title = models.CharField(max-length=250)
