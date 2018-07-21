3a857723e66ccb04dece1957318dd175d727bf52
supriadi-yusuf <supriadi96@gmail.com>
2018-07-21 16:52:58 +0700

    add string representation for model Album in music app

--- music/models.py
+++ music/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Album(models.Model):
-    artis = models.CharField(max_length=250)
+    artist = models.CharField(max_length=250)
     title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
     logo = models.CharField(max_length=1000)
+
+    def __str__(self): # string representation of object
+        return self.artist + " - " + self.title
 
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
