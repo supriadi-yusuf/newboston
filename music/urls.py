84f6a86c13bd58992147722590c45b50a2f4a20d
supriadi-yusuf <supriadi96@gmail.com>
2018-07-22 08:12:05 +0700

    fix url for /music/id/.list all album in music app

--- music/urls.py
+++ music/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     #url(r'^admin/', admin.site.urls),
     #url(r'^music/', include('music.urls')),
     url(r'^$', views.albumList, name='album_list'),
-    url(r'^(?P<album_id>\d+)$', views.albumDetail, name='album_detail'),
+    url(r'^(?P<album_id>\d+)/$', views.albumDetail, name='album_detail'),
 
 ]
--- music/views.py
+++ music/views.py
@@ -1,9 +1,17 @@
 from django.http import HttpResponse
+from .models import Album
 #from django.shortcuts import render
 
 # view is simple function/class that returns html
 def albumList(request):
-    return HttpResponse("<h1>This is the music app homepage</h1>")
+    html = ''
+
+    all_albums = Album.objects.all()
+    for album in all_albums:
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href="' + url + '">' + album.title + '</a><br>'
+
+    return HttpResponse(html)
 
 def albumDetail(request, album_id):
     return HttpResponse("<h1>Detail for music with id=" + str(album_id) + "</h1>")
