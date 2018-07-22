2762da384efcab02f9e076f06c373f697221f474
supriadi-yusuf <supriadi96@gmail.com>
2018-07-22 10:22:19 +0700

    use django shortcut render instead of loader

--- music/views.py
+++ music/views.py
@@ -1,17 +1,12 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Album
-#from django.shortcuts import render
 
 # view is simple function/class that returns html
 def albumList(request):
     all_albums = Album.objects.all()
-    template = loader.get_template('music/album_list.html')
-    context = {
-        'all_albums' : all_albums,
-    }
-
-    return HttpResponse(template.render(context,request))
+    context = { 'all_albums' : all_albums }
+    return render( request, 'music/album_list.html', context)
 
 def albumDetail(request, album_id):
     return HttpResponse("<h1>Detail for music with id=" + str(album_id) + "</h1>")
