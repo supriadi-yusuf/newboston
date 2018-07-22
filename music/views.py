dd0c1f2a734180264145e7b38a2cc395e122f9d6
supriadi-yusuf <supriadi96@gmail.com>
2018-07-22 10:08:50 +0700

    add template to music app to display album list

--- music/views.py
+++ music/views.py
@@ -1,17 +1,17 @@
 from django.http import HttpResponse
+from django.template import loader
 from .models import Album
 #from django.shortcuts import render
 
 # view is simple function/class that returns html
 def albumList(request):
-    html = ''
-
     all_albums = Album.objects.all()
-    for album in all_albums:
-        url = '/music/' + str(album.id) + '/'
-        html += '<a href="' + url + '">' + album.title + '</a><br>'
+    template = loader.get_template('music/album_list.html')
+    context = {
+        'all_albums' : all_albums,
+    }
 
-    return HttpResponse(html)
+    return HttpResponse(template.render(context,request))
 
 def albumDetail(request, album_id):
     return HttpResponse("<h1>Detail for music with id=" + str(album_id) + "</h1>")
