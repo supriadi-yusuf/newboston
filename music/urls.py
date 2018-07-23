8104d34a02910d2602284e5575ec1fe8d97408c4
supriadi-yusuf <supriadi96@gmail.com>
2018-07-23 09:16:29 +0700

    remove hard code url then replace it with named url in album list

--- music/urls.py
+++ music/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import url
 from . import views
 
-app_name = 'music_app' # add name space to url name for this app
-
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
     #url(r'^music/', include('music.urls')),
