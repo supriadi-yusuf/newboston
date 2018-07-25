ba5ebcd86c9fb84546ff4acc0653fe12e5a22273
supriadi-yusuf <supriadi96@gmail.com>
2018-07-25 16:15:15 +0700

    use generic view (class based view) instead of function based view

--- music/urls.py
+++ music/urls.py
@@ -6,8 +6,8 @@ app_name = "music_app" # this is url name space
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
     #url(r'^music/', include('music.urls')),
-    url(r'^$', views.albumList, name='album_list'),
-    url(r'^(?P<album_id>\d+)/$', views.albumDetail, name='album_detail'),
-    url(r'^(?P<album_id>\d+)/favorite/$', views.songFavorite, name='song_favorite'),
+    url(r'^$', views.IndexView.as_view(), name='album_list'),
+    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='album_detail'),
+    #url(r'^(?P<album_id>\d+)/favorite/$', views.songFavorite, name='song_favorite'),
 
 ]
--- music/views.py
+++ music/views.py
@@ -1,27 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.views import generic
 from .models import Album, Song
 
-# view is simple function/class that returns html
-def albumList(request):
-    all_albums = Album.objects.all()
-    context = { 'all_albums' : all_albums }
-    return render( request, 'music/album_list.html', context)
+class IndexView(generic.ListView): # there is no rule for class name here
+    template_name = 'music/album_list.html'
 
-def albumDetail(request, album_id):
-    # album = Album.objects.get(pk=album_id)
-    album = get_object_or_404( Album, pk=album_id)
-    return render( request, 'music/album_detail.html', { 'album':album})
+    def get_queryset(self):
+        return Album.objects.all()
+    #model = Album
 
-def songFavorite( request, album_id):
-    album = get_object_or_404( Album, pk=album_id)
-    try:
-        selected_song = album.song_set.get(pk=request.POST['song'])
-    except ( KeyError, Song.DoesNotExist):
-        return render( request, 'music/album_detail.html',
-                        { 'album':album,
-                          'error_message': 'You did not select a valid song'
-                        })
-    else:
-        selected_song.is_favorite = True
-        selected_song.save()
-        return render( request, 'music/album_detail.html', { 'album':album})
+class DetailView(generic.DetailView): # there is no rule for class name here
+    model = Album
+    template_name = 'music/album_detail.html'
